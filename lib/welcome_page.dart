be3ccf0fc5d20825ea18d87272dfe9863385f330
mohamed fahad <fahadcareer29@gmail.com>
2024-07-26 15:59:14 +0530

    Merge branch 'main' of https://github.com/fahadcareeer/MeeranaBanking

--- lib/welcome_page.dart
+++ lib/welcome_page.dart
@@ -11,7 +11,7 @@ class WelcomePage extends StatelessWidget {
     final hour = DateTime.now().hour;
     if (hour < 12) {
       return 'Good Morning,';
-    } else if (hour < 18) {
+    } else if (hour < 17) {
       return 'Good Afternoon,';
     } else {
       return 'Good Evening,';
@@ -37,7 +37,7 @@ class WelcomePage extends StatelessWidget {
                   ),
                 ),
                 const Text(
-                  'Abdullah!',
+                  'Admin !',
                   style: TextStyle(
                     fontSize: 24,
                     fontWeight: FontWeight.bold,
