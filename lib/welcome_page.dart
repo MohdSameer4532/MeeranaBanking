90d25fad7f46f9207af5bfa944157daa7db94470
Md Askar <mohamedaskar7242@gmail.com>
2024-07-26 15:57:02 +0530

    credit result updated

--- lib/welcome_page.dart
+++ lib/welcome_page.dart
@@ -37,7 +37,7 @@ class WelcomePage extends StatelessWidget {
                   ),
                 ),
                 const Text(
-                  'Abdullah!',
+                  'Admin!',
                   style: TextStyle(
                     fontSize: 24,
                     fontWeight: FontWeight.bold,
